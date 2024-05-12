893dc98fe457adb3a39861e158abe158864eb305
TaewookKwon <145336818+TaewookKwon@users.noreply.github.com>
2024-05-12 22:10:48 +0900

    Update joystick.py
    - pid 스티어링 제어 함수 추가
    구성:
    1. Kp: P 게인
    2. Ki: I 게인
    3. Kd: D 게인
    4. Ka: Anti-windup 게인
    
    - 조이스틱(6번 버튼) 으로 자율주행 시
    드라이빙 모드 / 제어기 Gain 튜닝 모드 변경 기능 추가
    
    제어기 Gain 튜닝 모드 On 시:
    Y(증가) A(감소) : Gain 값 변경, 0.0001
    B(증가), X(감소): Gain number 변경
    Gain number: 0(Kp), 1(Ki), 2(Kd), 3(Ka)
    
    - 카메라 화면의 중점 x축 조정 필요
    현재 추정값: 445

--- joystick.py
+++ joystick.py
@@ -7,6 +7,11 @@ from typing import Sequence
 from jetracer.nvidia_racecar import NvidiaRacecar
 import sys
 from pathlib import Path
+import torch
+import torchvision
+from torchvision import models
+from PIL import Image
+from cnn.center_dataset import TEST_TRANSFORMS
 
 sys.path.append('/home/ircv6/HYU-2024-Embedded/week07/Camera')
 import camera
@@ -25,7 +30,7 @@ sensor_id = 0
 downscale = 2
 width, height = (1280, 720)
 _width, _height = (width // downscale, height // downscale)
-frame_rate = 30
+frame_rate = 2
 flip_method = 0
 contrast = 1.3
 brightness = 0.2
@@ -52,6 +57,11 @@ gstreamer_pipeline = (
 )
 
 
+model = models.alexnet(num_classes=2, dropout=0.0)
+device = torch.device('cuda')
+model.load_state_dict(torch.load('/home/ircv6/HYU-2024-Embedded/jetracer/road_following_model_taewook.pth'))
+model = model.to(device)
+
 save_path = str('record')
 # For headless mode
 os.environ["SDL_VIDEODRIVER"] = "dummy"
@@ -60,6 +70,7 @@ pygame.init()
 pygame.joystick.init()
 
 car.throttle_gain = 0.4
+car.steering_gain = -1
 
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
@@ -71,72 +82,252 @@ throttle_range = (-0.5, 0.5)
 # 조이스틱 버튼 플래그
 flag_4_1=False
 flag_4_2=False
+
 flag_0_1=False
 flag_0_2=False
-#1번 : 카메라 녹화 버튼
+
+# 1번 : 카메라 녹화 버튼
 flag_1_1=False
 flag_1_2=False
-#6번 : 스티어링 왼쪽 보정
+
+# 3번 : 게인 모드에서 조절 버튼
+flag_3_1=False
+flag_3_2=False
+
+# 6번 : 자율 주행
 flag_6_1=False
 flag_6_2=False
-#7번 : 스티어링 오른쪽 보정
+
+#7번 : Throttle / 제어 Gain 설정 변경 
 flag_7_1=False
 flag_7_2=False
+
+automode = False
 collecting = False
-cnt = 1
+cnt = 0
+gain_Tuning = False
+gain_number = 0
+
+## 제어 파라미터
+th_control=0.45
+deadzone=10
+K_p=0.007
+K_i=0
+K_d=0
+K_a=0
+error_I=0
+
+def steering_control(reference,K_p,K_i,K_d,K_a):
+    global error_I
+    error_feedback = reference
+    error_P = error_feedback
+    error_I += error_feedback
+    input_pid = K_p*error_P + K_i*error_I
+
+    input_windup = max(-1,min(1,input_pid))
+    error_windup = input_pid - input_windup
+    error_I -= K_a *error_windup
+    return input_windup
+
 while running:
     pygame.event.pump()
+    #자율 주행 모드
+    if automode:
+        ret, frame = cam.cap[0].read()
+        # throttle = 0.4
+        # throttle = max(throttle_range[0], min(throttle_range[1], throttle))
+        # car.throttle = throttle
+        if ret:
+            pil_image = Image.fromarray(cv2.cvtColor(frame,cv2.COLOR_BGR2RGB))
+            width, height = pil_image.size
+            with torch.no_grad():
+                img = TEST_TRANSFORMS(pil_image).to(device)
+                img = img[None, ...]
+                output = model(img).detach().cpu().numpy()
+                x, y = output[0]
+                x = (x / 2 + 0.5) * width
+                y = (y / 2 + 0.5) * height
 
-    throttle = -joystick.get_axis(1)
-    throttle = max(throttle_range[0], min(throttle_range[1], throttle))
-    car.throttle = throttle
-    steering = joystick.get_axis(2)
-    car.steering = steering-0.296
+                ## 조향 제어 로직(pid)
+                reference=x-445
+                steering_input = steering_control(reference,K_p,K_i,K_d,K_a)
+                car.steering = steering_input
+                
+                if gain_Tuning:
+                    car.throttle = 0
+                    print("Tuning mode, gain_num: {} K_p: {}, K_i: {} K_d: {} K_a: {} y_error: {} ".format(gain_number, K_p, K_i,K_d,K_a,reference))
+                else:
+                    car.throttle = th_control
+                    print("Driving mode, thro: {:2f} steer input: {} K_p: {} x: {} ".format(th_control, steering_input,K_p,reference))
+                # # 기존 제어 로직
+                # car.throttle=th_control
+                # error_feedback = x-640
+                # if error_feedback > deadzone:
+                #     car.steering = 0.7
+                #     #car.throttle=0.35
+                #     print("Car is on left side")
+                # elif error_feedback < -deadzone:
+                #     car.steering = -1
+                #     #car.throttle=0.32
+                #     print("Car is on right side")
+                # else:
+                #     car.steering = -0.3
+                #     #car.throttle=0.32
+                #     print("Car is on center line")
 
+                #print(x, y,"Steering input: {}".format(steering_input()))
 
-    # print("thro: {:2f} steer: {:2f} th_gain: {:2f} ".format(throttle, steering,car.throttle_gain))
+    #조이스틱으로 조작
+    else: 
+        throttle = -joystick.get_axis(1)
+        throttle = max(throttle_range[0], min(throttle_range[1], throttle))
+        car.throttle = throttle
+        steering = joystick.get_axis(2)
+        car.steering = steering
+        print("thro: {:2f} steer input: {} ".format(throttle, steering))
+    #print("thro: {:2f} steer: {:2f} th_gain: {:2f} deadzone: {} ".format(throttle, car.steering_gain*steering,car.throttle_gain,deadzone))
+    
+    
     if joystick.get_button(11): # start button
         running = False
 
-
-    # Throttle Gain 증가
-    flag_4_1 = joystick.get_button(4)
-    if flag_4_1 and not flag_4_2:
-        car.throttle_gain+=0.02
-    flag_4_2=flag_4_1   
-
-    # Throttle Gain 감소
-    flag_0_1 = joystick.get_button(0)
-    if flag_0_1 and not flag_0_2:
-        car.throttle_gain-=0.02
-    flag_0_2=flag_0_1
-
-    # 스티어링 왼쪽 보정
-    flag_6_1 = joystick.get_button(6)
-    if flag_6_1 and not flag_6_2:
-        car.steering = -0.3
-        print("steering left")
-        print("steering gain :", car.steering_gain)
-        print("steering :", car.steering)
-        print("steering offset:", car.steering_offset)
-    flag_6_2=flag_6_1   
-
-    # 스티어링 오른쪽 보정
     flag_7_1 = joystick.get_button(7)
     if flag_7_1 and not flag_7_2:
-        car.steering = 0.3
-        print("steering right")
-        print("steering gain :", car.steering_gain)
-        print("steering :", car.steering)
-        print("steering offset:", car.steering_offset)
-    flag_7_2=flag_7_1
+        gain_Tuning = not gain_Tuning
+        flag_4_1=flag_4_2=False #4번 버튼 초기화    
+    flag_7_2=flag_7_1 
+
+    if gain_Tuning:
+        # 튜닝할 게인 번호 조정: 0은 K_p, 1은 K_i, 2는 K_d, 3은 K_a
+        # 게인 번호 증가
+        flag_1_1 = joystick.get_button(1)
+        if flag_1_1 and not flag_1_2:
+            gain_number= (gain_number+1)%4
+        flag_1_2=flag_1_1
+
+        flag_3_1 = joystick.get_button(3)
+        if flag_3_1 and not flag_3_2:
+            gain_number= (gain_number-1)%4
+        flag_3_2=flag_3_1
+
+        if gain_number==0:
+            # K_p 증가
+            flag_4_1 = joystick.get_button(4)
+            if flag_4_1 and not flag_4_2:
+                K_p += 0.001
+                K_p = min(10,K_p)
+            flag_4_2=flag_4_1   
+
+            # K_p 감소
+            flag_0_1 = joystick.get_button(0)
+            if flag_0_1 and not flag_0_2:
+                K_p -= 0.001
+                K_p = max(0,K_p)
+            flag_0_2=flag_0_1
+
+        elif gain_number==1:
+            # K_i 증가
+            flag_4_1 = joystick.get_button(4)
+            if flag_4_1 and not flag_4_2:
+                K_i += 0.001
+                K_i = min(10,K_i)
+            flag_4_2=flag_4_1   
+
+            # K_i 감소
+            flag_0_1 = joystick.get_button(0)
+            if flag_0_1 and not flag_0_2:
+                K_i -= 0.001
+                K_i = max(0,K_i)
+            flag_0_2=flag_0_1
+
+        elif gain_number==2:
+            # K_d 증가
+            flag_4_1 = joystick.get_button(4)
+            if flag_4_1 and not flag_4_2:
+                K_d += 0.001
+                K_d = min(10,K_d)
+            flag_4_2=flag_4_1   
+
+            # K_d 감소
+            flag_0_1 = joystick.get_button(0)
+            if flag_0_1 and not flag_0_2:
+                K_d -= 0.001
+                K_d = max(0,K_d)
+            flag_0_2=flag_0_1
+        else: # gain_number==3
+            # K_a 증가
+            flag_4_1 = joystick.get_button(4)
+            if flag_4_1 and not flag_4_2:
+                K_a += 0.001
+                K_a = min(10,K_a)
+            flag_4_2=flag_4_1   
+
+            # K_a 감소
+            flag_0_1 = joystick.get_button(0)
+            if flag_0_1 and not flag_0_2:
+                K_a -= 0.001
+                K_a = max(0,K_a)
+            flag_0_2=flag_0_1
+
+    else:
+        #녹화버튼 설정
+        if joystick.get_button(1) and not flag_1_1:
+            collecting = not collecting
+            flag_1_1 = True
+            if collecting:
+                print("Starting image Collection...")
+            else:
+                print("Stopping image Collection...")
+                
+        elif not joystick.get_button(1) and flag_1_1:
+            flag_1_1 = False
+
+        # 자율주행 모드 시 throttle 증가
+        flag_4_1 = joystick.get_button(4)
+        if flag_4_1 and not flag_4_2:
+            th_control += 0.01
+            th_control = min(0.7,th_control)
+        flag_4_2=flag_4_1   
+
+        # 자율주행 모드 시 throttle 감소
+        flag_0_1 = joystick.get_button(0)
+        if flag_0_1 and not flag_0_2:
+            th_control -= 0.01
+            th_control = max(0,th_control)
+
+
+    # # Throttle Gain 증가
+    # flag_4_1 = joystick.get_button(4)
+    # if flag_4_1 and not flag_4_2:
+    #     car.throttle_gain += 0.02
+    # flag_4_2=flag_4_1   
+
+    # # Throttle Gain 감소
+    # flag_0_1 = joystick.get_button(0)
+    # if flag_0_1 and not flag_0_2:
+    #     car.throttle_gain -= 0.02
+    # flag_0_2=flag_0_1
+
+    # # Deadzone 증가
+    # flag_4_1 = joystick.get_button(4)
+    # if flag_4_1 and not flag_4_2:
+    #     deadzone += 10
+    #     deadzone = min(640,deadzone)
+    # flag_4_2=flag_4_1   
+
+    # # Deadzone 감소
+    # flag_0_1 = joystick.get_button(0)
+    # if flag_0_1 and not flag_0_2:
+    #     deadzone-= 10
+    #     deadzone = max(0,deadzone)
+    # flag_0_2=flag_0_1
 
     #gstreamer 파이프라인 설정
     sensor_id = 0
     downscale = 2
     width, height = (1280, 720)
     _width, _height = (width // downscale, height // downscale)
-    frame_rate = 30
+    frame_rate = 2
     flip_method = 0
     contrast = 1.3
     brightness = 0.2
@@ -161,17 +352,6 @@ while running:
             _height,
         )
     )
-    #녹화 
-    if joystick.get_button(1) and not flag_1_1:
-        collecting = not collecting
-        flag_1_1 = True
-        if collecting:
-            print("Starting image Collection...")
-        else:
-            print("Stopping image Collection...")
-            
-    elif not joystick.get_button(1) and flag_1_1:
-        flag_1_1 = False
 
     #사진 수집이 활성화된 경우
     cam_save_path = Path(cam.save_path)
@@ -186,6 +366,18 @@ while running:
             print(f"Saved frame at {timestamp}")
         else:
             print("Failed to capture frame")
+
+    # 자율주행
+    if joystick.get_button(6) and not flag_6_1:
+        automode = not automode
+        flag_6_1 = True
+        if automode:
+            print("AutoDriving Start...")
+        else:
+            print("AudoDriving Stopped...")
+    elif not joystick.get_button(6) and flag_6_1:
+        flag_6_1 = False
+    
 camera.cap[0].release()
 cv2.destroyAllWindows()
-pygame.quit()        
+pygame.quit()     
